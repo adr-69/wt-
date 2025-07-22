015da1a2470bdf441e9bc1332e8870dc00bd8763
adr-69 <bindgarenacod@gmail.com>
2025-07-22 16:08:40 +0800

    update

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const openPopup = document.querySelectorAll('.topicBTNpop');
 const closePopup = document.getElementById('closePopup');
 const popup = document.getElementById('popup')
-
 openPopup.forEach(btn => {
     btn.onclick = () => {
         popup.style.display = 'flex';
     };
 });
 closePopup.onclick = () => {
-    popup.style.display = 'none'
+    popup.style.display = 'none';
 }
